0f5ca1a8501fa6b2346332259a0ee8f78c773fc3
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-07-12 14:09:46 -0400

    feat: incorporando Choices en el panel de administración

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
-admin.site.register(Question)
+class ChoicesInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information (Esto es editable y es el título del conjunto de campos)", 
+            {  
+             "fields": ["pub_date"],
+             "classes": ["collapse"],
+            }
+         ),
+    ]
+    inlines = [ChoicesInline]
+
+
+admin.site.register(Question, QuestionAdmin)
